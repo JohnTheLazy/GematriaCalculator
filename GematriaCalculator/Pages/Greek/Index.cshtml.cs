27b92d580355f077392eccd18f91e0646071591a
ethanium <ethanium@live.com>
2022-10-10 21:12:07 +0800

    Added Greek Page

---
+++ GematriaCalculator/Pages/Greek/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GematriaCalculator.Pages.Greek
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GematriaCalculator.Data.StrongsDbContext _context;
+
+        public IndexModel(GematriaCalculator.Data.StrongsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Greek> Greeks { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Greeks != null)
+            {
+                Greeks = await _context.Greeks.ToListAsync();
+            }
+        }
+    }
+}
